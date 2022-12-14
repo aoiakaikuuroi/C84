326672cc9459c73c739bdbd4f2c8f2d9775699e4
EmilieYamada123 <109628271+aoiakaikuuroi@users.noreply.github.com>
2022-12-14 18:24:19 -0300

    Add files via upload

---
+++ Projeto-84/main.js
@@ -0,0 +1,66 @@
+Canvas = getElementById("myCanvas");
+ctx.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image();
+	img_imgTag.onload = uploadimg;
+	img_imgTag.src = img_image;
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown" , my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		{
+            aplhabetkey();
+            document.getElementById("d1").innerHTML = "você precionou uma tecla alfabeto";
+            console.log("alphabet key");
+        }
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	     }
+}
+
+function aplhabetkey()
+{
+	img_image = "alfabeto.png";
+    add();
+}
+function numberkey()
+{
+	img_image = "número.png";
+    add();
+}
+function arrowkey()
+{
+    img_image = "direcional.png";
+    add();
+}
+function specialkey()
+{
+	img_image = "especial.png";
+    add();
+}
+function otherkey()
+{
+	img_image="outras.png";
+	add();
+}
